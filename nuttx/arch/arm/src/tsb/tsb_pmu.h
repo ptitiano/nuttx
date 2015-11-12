014b9a750e44ee38647d46999410232cd4a750b4
Patrick Titiano <ptitiano@baylibre.com>
2015-11-12 14:25:47 +0100

    tsb: es2: pmu: register definitions
    TSB Bridge PMU (Power Management Unit) register definitions.
    
    Testing Done: compile with no warning/error
    Bug Number:   SW-1149
    Review URL:
    Reviewed by:
    
    Signed-off-by: Patrick Titiano <ptitiano@baylibre.com>

---
+++ nuttx/arch/arm/src/tsb/tsb_pmu.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2015 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @file    arch/arm/src/tsb/tsb_pmu.h
+ * @brief   TSB PMU (Power Management Unit) Register Definitions
+ * @author  Patrick Titiano
+ */
+
+#ifndef __TSB_PMU_H__
+#define __TSB_PMU_H__
+
+#include "chip.h"
+
+#ifdef CONFIG_TSB_CHIP_REV_ES2
+#include "tsb_pmu_es2.h"
+#endif
+
+
+#endif /* __TSB_PMU_H__ */
+
